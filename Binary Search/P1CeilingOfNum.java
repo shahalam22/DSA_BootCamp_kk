6b7f07c4327da70988d51dfbc3d6f6f74943ba28
SHAH ALAM <shahalamabir21@gmail.com>
2023-01-29 16:25:03 +0600

    Create P1CeilingOfNum.java

---
+++ Binary Search/P1CeilingOfNum.java
@@ -0,0 +1,33 @@
+public class P1CeilingOfNum{
+
+  public static void main(String[] args) {
+    int[] num = {1,2,4,7,9,12,15,18,21,24,27,32};
+    int target = 5;
+    int ans = binarySearch(num, target);
+    System.out.print(ans);
+  }
+  
+  static int binarySearch(int[] arr, int target){
+    int start = 0;
+    int end = arr.length -1;
+    
+    if(target > arr[end]) return -1;
+    
+    while(start<=end){
+    
+      int mid = start + ((end - start)/2);    
+    
+      if(target<arr[mid]){
+        end = mid - 1;
+      }
+      else if(target>arr[mid]){
+        start = mid + 1;
+      }
+      else{
+        return mid;
+      }
+    }
+    return start;
+  }
+  
+}
