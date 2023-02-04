0357d90c26e947b3363c406ce520c6071116385c
SHAH ALAM <shahalamabir21@gmail.com>
2023-02-04 10:50:08 +0600

    Create P5SetMisMatch.java

---
+++ Sorting/P5SetMisMatch.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class SetMisMatch {
+
+  public static void main(String[] args) {
+    int[] arr = {2,1,1,3,4,5,6};
+    System.out.println(Arrays.toString(duplicate(arr)));
+  }
+  
+  static int[] duplicate(int[] arr){
+    int i;
+    cyclicSort(arr);
+    
+    for(i=0; i<arr.length; i++){
+      if(arr[i]!=i+1){
+        break;
+      }
+    }
+    return new int[] {arr[i], i+1};
+  }
+  
+  static void cyclicSort(int[] arr){
+    int i=0;
+    while(i<arr.length){
+      int correct = arr[i]-1;
+      if(arr[i]!=arr[correct]){
+        swap(arr, i, correct);
+      }
+      //43021-13024-31024-21034-01234
+      else{
+        i++;
+      }
+    }
+  }
+   
+  static void swap(int[] arr, int start, int end){
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+  }
+}
