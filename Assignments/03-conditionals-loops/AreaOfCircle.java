2f74ff57eafd6865a8f27740a88356fcb6297995
SHAH ALAM <shahalamabir21@gmail.com>
2023-02-06 23:20:11 +0600

    Create AreaOfCircle.java

---
+++ Assignments/03-conditionals-loops/AreaOfCircle.java
@@ -0,0 +1,11 @@
+//Area Of Circle Java Program
+
+public class AreaOfCircle {
+
+  public static void main(String[] args) {
+    double radius = 2.5f;
+    double area = 3.1416*radius*radius;
+    System.out.printf("Area of circle is : %.2f", area);
+  }
+
+}
